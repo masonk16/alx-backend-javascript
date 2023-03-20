ec873a134d781c43b3879ed8dec4b17c5dc6e7a8
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-20 11:18:32 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mason Mapfundematsva <https://github.com/masonk16>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
